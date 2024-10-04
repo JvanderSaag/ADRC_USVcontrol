f8119eafe47e4a6383be9c8e0ab549b5bfa685b3
Jelmer van der Saag <jelmer.van.der.saag@demcon.com>
2024-10-04 14:37:47 +0200

    Updated docstrings of system class

--- systems/Vessel_SISO.py
+++ systems/Vessel_SISO.py
@@ -1,20 +1,18 @@
 class SISOVesselSystem:
     """
-    A class to represent a Single Input Single Output (SISO) USV system.  
-    Considers a typical surge motion model for a USV, removing coupling effects from other degrees of freedom.
+    A class used to represent a Single Input Single Output (SISO) vessel system.
+
+    Attributes:
+        m11 (float): Mass of the vessel.
+        X_u (float): Linear damping coefficient.
+        X_absuu (float): Quadratic damping coefficient.
+        position (float): Position of the vessel.
+        velocity (float): Velocity of the vessel.
     """
-    
     def __init__(self):
         """
         Constructs all the necessary attributes for the SISOVesselSystem object. 
         Example parameters are taken from "Modelling and Trajectory Planning for a Small-Scale Surface Ship by Steen & Zetterqvist
-        
-        Parameters:
-        -----------
-        mass : float, optional
-            Mass of the vessel (default is 1.0).
-        damping_coefficient : float, optional
-            Damping coefficient of the vessel (default is 0.1).
         """
         # Vessel parameters
         self.m11 = 17.06
@@ -22,32 +20,25 @@ class SISOVesselSystem:
         self.X_absuu = -0.79
 
         # Vessel state variables
-        self.position = 0  # Position (x)
-        self.velocity = 0  # Velocity (xdot)
+        self._position = 0  # Position (x)
+        self._velocity = 0  # Velocity (xdot)
     
     def step(self, control_input, disturbance, dt):
         """
         Updates the state of the vessel system for a given time step.
         
-        Parameters:
-        -----------
-        control_input : float
-            The control input applied to the vessel.
-        disturbance : float
-            The external disturbance acting on the vessel.
-        dt : float
-            The time step for the simulation.
-        
+        Args:
+            control_input (float): Control input to the system.
+            disturbance (float): Disturbance acting on the system.
+            dt (float): Time step duration.
         Returns:
-        --------
-        float
-            The updated position of the vessel.
+            float: New position of the vessel after the time step.
         """
         # Update acceleration based on the control input and disturbance
-        acceleration = 1/self.m11 * (-self.X_u * self.velocity + self.X_absuu * abs(self.velocity) * self.velocity + control_input + disturbance)
-        
+        acceleration = 1/self.m11 * (-self.X_u * self._velocity + self.X_absuu * abs(self._velocity) * self._velocity + control_input + disturbance)
+
         # Update velocity and position using Euler integration
-        self.velocity += acceleration * dt
-        self.position += self.velocity * dt
+        self._velocity += acceleration * dt
+        self._position += self._velocity * dt
         
-        return self.position
+        return self._position
